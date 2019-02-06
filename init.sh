4d698744474a8ffa90c772f19dd44b589939e89b
Eric <ericzhao96@hotmail.com>
2019-02-06 16:40:09 +0800

    [add] auto install Plugin

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vimrc ~/.vimrc
 cp ./vimrc.bundles ~/.vimrc.bundles
+
 sudo apt-get install fonts-powerline
 
-echo ">vim >:PluginInstall\n"
+vim +PluginInstall +qall
+
+echo "Vim init finish!\n"
